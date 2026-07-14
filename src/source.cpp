667b24e7762587caaab2c98c4f98a7f4c7cf0611
yxhyx <gthemasterofgam3s@gmail.com>
2026-07-14 20:10:09 +0100

    add todo

--- src/source.cpp
+++ src/source.cpp
@@ -2,6 +2,20 @@
 #include <windows.h>
 
 #include "game/Game.h"
+
+/*	todo
+* add castling, en passant
+* add ui for promotion 
+* properly fix logging
+* properly setup sounds
+* add menu and exit screen
+* 
+* for later:
+* add bots (configure algorithms)
+* add hints
+* 
+*/
+
 int main()
 {
 	//hide cmd
